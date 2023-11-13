545119ae13915699c1f477a637c2018be59d6b2c
Arthur Chernyshev <artur.chernyshev@rtkit.ru>
2023-11-13 18:27:07 +0300

    Fix session

--- app/repository/base_repository.py
+++ app/repository/base_repository.py
@@ -1,16 +1,15 @@
 from pydantic import BaseModel
 from sqlalchemy import delete, insert, select, update
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.base import Base
 
 
 class BaseRepository:
-    def __init__(self: "BaseRepository", model: Base, session: Session):
+    def __init__(self: "BaseRepository", model: Base):
         self.model = model
-        self.session = session
 
-    async def get_items(self: "BaseRepository", query=None, offset=None, limit=None, order_by=None):
+    async def get_items(self: "BaseRepository", session: AsyncSession, query=None, offset=None, limit=None, order_by=None):
         if query is None:
             query = select(self.model)
 
@@ -23,27 +22,27 @@ class BaseRepository:
         if order_by is not None:
             query = query.order_by(*order_by)
 
-        return self.session.execute(query)
+        return await session.execute(query)
 
-    async def get_item(self: "BaseRepository", obj_id: int):
-        return self.session.get(self.model, obj_id)
+    async def get_item(self: "BaseRepository", session: AsyncSession, obj_id: int):
+        return await session.get(self.model, obj_id)
 
-    async def create_item(self: "BaseRepository", obj_in: BaseModel):
-        result = self.session.execute(
+    async def create_item(self: "BaseRepository", session: AsyncSession, obj_in: BaseModel):
+        result = await session.execute(
             insert(self.model).values(**obj_in.model_dump()).returning(self.model),
         )
-        self.session.commit()
+        await session.commit()
         return result.scalar_one()
 
-    async def update_item(self: "BaseRepository", obj_id: int, obj_upd: BaseModel) -> Base:
+    async def update_item(self: "BaseRepository", session: AsyncSession, obj_id: int, obj_upd: BaseModel) -> Base:
         update_data = obj_upd.model_dump()
-        result = self.session.execute(
+        result = await session.execute(
             update(self.model).where(self.model.id == obj_id).values(**update_data).returning(self.model),
         )
-        self.session.commit()
+        await session.commit()
         return result.scalar_one()
 
-    async def delete_item(self: "BaseRepository", obj_id: int):
-        result = self.session.execute(delete(self.model).where(self.model.id == obj_id))
-        self.session.commit()
+    async def delete_item(self: "BaseRepository", session: AsyncSession, obj_id: int):
+        result = await session.execute(delete(self.model).where(self.model.id == obj_id))
+        await session.commit()
         return result
